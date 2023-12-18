b101ec7b3ad032387bf87682aa219efc99c358fb
Niegil-Francis <niegilfrancis14@gmail.com>
2023-12-17 23:22:23 -0500

    Added files

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# simplifying_music_transformer
-This repository contains the final project of Deep Learning Fall 2023.
+# Music Transformer Tutorial
+
+This repository contains the final project of Deep Learning Fall 2023. Please refer to Tutorial.ipynb. It contains all the code for this project.
---
+++ hparams.py
@@ -0,0 +1,38 @@
+
+from torch import cuda, device as d
+from vocabulary import vocab_size
+
+# get device
+if cuda.is_available():
+    dev = "cuda:0"
+else:
+    dev = "cpu"
+device = d(dev)
+
+# default hparams for the model
+hparams = {
+    "d_model": 128,
+    "num_layers": 3,
+    "num_heads": 8,
+    "d_ff": 512,
+    "max_rel_dist": 1024,
+    "max_abs_position": 0,
+    "vocab_size": vocab_size,
+    "bias": True,
+    "dropout": 0.1,
+    "layernorm_eps": 1e-6
+}
+
+# hparams for TF model - significantly larger
+hparams_large = {
+    "d_model": 256,
+    "num_layers": 6,
+    "num_heads": 8,
+    "d_ff": 1024,
+    "max_rel_dist": 1024,
+    "max_abs_position": 0,
+    "vocab_size": vocab_size,
+    "bias": True,
+    "dropout": 0.1,
+    "layernorm_eps": 1e-6
+}
